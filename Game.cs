a6de25f26d3100c1571ef6ff2672b5b6b56272d1
Lenny2k20 <S00196446@mail.itsligo.ie>
2021-05-11 11:01:11 +0100

    Step 5.

--- Game.cs
+++ Game.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Eric_Leonard_S00196446
 {
-    class Game
+    public class Game
     {
         public string Name { get; set; }
 
-        public decimal Metacritic { get; set; }
+        public double Metacritic { get; set; }
 
         public string Description { get; set; }
 
@@ -25,11 +26,30 @@ namespace Eric_Leonard_S00196446
 
         }
 
+        public Game(string name, decimal price, double metacritic, string description, string platform, string game_image)
+        {
+            Name = name;
+            Metacritic = metacritic;
+            Description = description;
+            Platform = platform;
+            Price = price;
+            Game_Image = game_image;
+        }
+
         public void DecreasePrice(double increase)
         {
             Price *= (decimal)(1 - increase);
         }
+
     }
 
+    public class GameDB : DbContext
+    {
+        public GameDB() : base("MyGameData") { }
 
+        public DbSet<Game> GameDetails { get; set; }
+    }
 }
+
+
+
