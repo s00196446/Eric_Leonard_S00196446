ad6cc419e99c64f0225c3a1f4450a2e3e4f9bcb4
Lenny2k20 <S00196446@mail.itsligo.ie>
2021-05-11 09:47:43 +0100

    Step 2.

---
+++ Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eric_Leonard_S00196446
+{
+    class Game
+    {
+        public string Name { get; set; }
+
+        public decimal Metacritic { get; set; }
+
+        public string Description { get; set; }
+
+        public string Platform { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Game_Image { get; set; }
+    }
+}
